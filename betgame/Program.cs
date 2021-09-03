9439cadbabb4e60524d578fbffcf6ce8d54d607a
praveen <praveenthakur388@gmail.com>
2021-09-03 18:06:37 +0530

    Add project files.

---
+++ betgame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace betgame
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            double odds = 0.75;
+            Console.WriteLine("Welcome to the casino.the odds are: "+ odds);
+            guy player = new guy() { cash = 100, name = "The player" };
+            player.writeMyInfo();
+            while (player.cash>0)
+            {
+                Console.Write("How much do you want to bet or press \"Enter\" to exit the casino:");
+                string howmuch = Console.ReadLine();
+                if (howmuch == "")
+                {
+                    Console.WriteLine("Thanks for Coming, Good Bie!");
+                    return;
+                }
+                if (int.TryParse(howmuch, out int amount))
+                {
+                    double number = random.NextDouble();
+                    if (number > odds)
+                    {
+                        amount *= 2;
+                        player.ReceiveCash(amount);
+                        Console.WriteLine("You Win: " + amount);
+                        player.writeMyInfo();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad luck, You Loss!");
+                        player.GiveCash(amount);
+                        player.writeMyInfo();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid input please enter a integer");
+                }
+            }
+            Console.WriteLine("You have no cash to play, Good bie!");
+        }
+    }
+}
---
+++ betgame/guy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace betgame
+{
+    class guy
+    {
+        public string name;
+        public int cash;
+       // methods will start from here       
+        public void writeMyInfo()
+        {
+            Console.WriteLine(name + " has " + cash + " rupees");
+        }
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(name + " says the money is not valid");
+            }
+            if (amount > cash)
+            {
+                Console.WriteLine(name + "says i don't have enough money");
+            }
+            cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("the amount is invalid");
+            }
+            else
+            {
+                cash += amount;
+            }
+        }
+    }
+}
